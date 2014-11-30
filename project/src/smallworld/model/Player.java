d70506b2458880628ae04ad358ecab8d22f1abe8
pierrickwagner <pierrick@estvideo.fr>
2014-11-30 12:12:10 +0100

    Classe Player : Implémentation de attack et simulateAttack, et plusieurs corrections

--- project/src/smallworld/model/Player.java
+++ project/src/smallworld/model/Player.java
@@ -20,12 +20,43 @@ public class Player {
 	
 	
 	
+	public Player()
+	{
+		points = 5;
+		availablePop = 0;
+		lands = new ArrayList<Land>();
+	}
 	
 	
 	//Methods
 	
-	public void attack(Land land)
+	public void attack(Land target)
 	{
+		int neededTroops = simulateAttack(target);
+		if(neededTroops != -1 && availablePop > 0)// Si l'attaque est possible
+		{
+			int sentTroops = Math.min(neededTroops, availablePop);
+			availablePop -= sentTroops;
+			
+			
+			int reinforcements = 0;
+			if(sentTroops < neededTroops)
+			{
+				reinforcements = throwDice();
+			}
+			
+			if(sentTroops + reinforcements >= neededTroops)//Attaque réussie
+			{
+				if(target.getOccupant() != null)
+					target.getOccupant().looseLand(target);
+				target.setOccupant(this);
+				target.setPopulation(sentTroops);
+				
+				lands.add(target);
+			}
+				
+		}
+		
 		
 	}
 	
@@ -60,6 +91,14 @@ public class Player {
 			tribeDeleted(previousTribe);
 		}
 		
+		for(Land l : lands)
+		{
+			l.setPopulation(1);
+			l.setOccupant(null);
+		}
+		lands.clear();
+		availablePop = 0;
+		
 		previousTribe = currentTribe;
 		currentTribe = null;
 	}
@@ -71,6 +110,7 @@ public class Player {
 	{
 		currentTribe = tribe;
 		points -= cost;
+		availablePop = tribe.getPopulation();
 	}
 	
 	
@@ -89,27 +129,38 @@ public class Player {
 				l.setPopulation(1);
 			}
 		}
-	}
-	
-	public int throwDice()
-	{
-		double rand = Math.random();
-		int result;
-		if(rand < 1d/6d)
-			result = 3;
-		else if(rand < 1d/3d)
-			result = 2;
-		else if(rand < 1d/2d)
-			result = 1;
-		else
-			result = 0;
 		
-		return result;
 	}
 	
-	public int simulateAttack()
+	public int simulateAttack(Land target)
 	{
-		return 0;
+		//TODO optimisation possible
+		
+		boolean legalMove = false;
+		
+		//Boucle qui vérifie que la cible soit bien l'un des lands adjacents à ceux possédés.
+		for(Land l : lands)
+		{
+			for(Land neighbor : l.getNeighbors())
+			{
+				if(neighbor == target)
+					legalMove = true;
+			}
+		}
+		
+		//On vérifie que le joueur ne s'attaque pas lui-meme
+		if(target.getOccupant() == this)
+			legalMove = false;
+		
+		
+		if(legalMove)
+		{
+			// Fonctionne dans le cas général, des conditions à rajouter pour les tribus / terrains particuliers
+			int neededTroops = 2;
+			neededTroops += target.getPopulation();
+		}
+		else
+			return -1; // Attaque impossible
 	}
 	
 	
@@ -117,6 +168,9 @@ public class Player {
 	
 	public void looseLand(Land l)
 	{
+		//Quand on perd une case on rècupère les jetons de cette case moins 1
+		availablePop += l.getPopulation() -1;
+		
 		lands.remove(l);
 	}
 	
@@ -168,5 +222,31 @@ public class Player {
 	public ArrayList<Land> getLands() {
 		return lands;
 	}
+	
+	
+	
+	
+	
+	
+	
+	//Dé
+	private static int throwDice()
+	{
+		double rand = Math.random();
+		int result;
+		if(rand < 1d/6d)
+			result = 3;
+		else if(rand < 1d/3d)
+			result = 2;
+		else if(rand < 1d/2d)
+			result = 1;
+		else
+			result = 0;
+		
+		return result;
+	}
+	
+	
+	
 	
 }
