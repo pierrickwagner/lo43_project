28d0192de283de77ee0412c6cc22883e867b5996
pierrickwagner <pierrick@estvideo.fr>
2014-12-11 08:31:56 +0100

    Deplacement d'ImpossibleAttackException dans le package smallworld.exceptions

---
+++ project/src/smallworld/exceptions/ImpossibleAttackException.java
@@ -0,0 +1,28 @@
+package smallworld.exceptions;
+
+public class ImpossibleAttackException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public enum Reason { FRIENDLY_FIRE, NOT_REACHABLE, NO_TROOPS, NOT_ENOUGH_TROOPS };
+	
+	
+	Reason reason;
+	
+	public ImpossibleAttackException(Reason reason)
+	{
+		this.reason = reason;
+	}
+	
+	
+	public Reason getReason()
+	{
+		return reason;
+	}
+	
+	
+}
