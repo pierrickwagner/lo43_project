3ec1b7ac0a2182d93ed65fbb01080adf496a56e2
pierrickwagner <pierrick@estvideo.fr>
2014-12-13 12:06:20 +0100

    Petits ajustements

--- project/src/smallworld/model/Bank.java
+++ project/src/smallworld/model/Bank.java
@@ -14,26 +14,26 @@ public class Bank implements TribeDeletedListener{
 	private ArrayList<Power> listOfPowers;
 	private ArrayList<Population> listOfPopulations;
 	private ArrayList<Tribe> AvailableTribes;
-	private String listOfDescriptionsPop[] = {"Leur connaissance du terrain leur permet d'être plus efficace pour se defendre. Il faut une unité de plus pour les attaquer.", 
-			"Etant d'une grande capacité d'adaptation ils peuvent avec 1 unité en - attaquer les labos", 
-			"Leur envie de s'établir sur un ordinateur est tellement grande que leur combativité en est stimulé ils ont besoin d'une unité en - pr attaquer les salles GI" , 
+	private String listOfDescriptionsPop[] = {"Leur connaissance du terrain leur permet d'être plus efficace pour se défendre. Il faut une unité de plus pour les attaquer.", 
+			"Étant d'une grande capacité d'adaptation ils peuvent attaquer les labos avec 1 unité en moins", 
+			"Leur envie de s'établir sur un ordinateur est tellement grande que leur combativité en est stimulée, ils ont besoin d'une unité en moins pour attaquer les salles GI" , 
 			"Leur charme absolu les rendent capable de rendre inutile 1 défenseur du terrain attaqué."
-			,"Leur rapidité a jugé leurs ennemis leur permet de se replier rapidement ils ne perdent pas de troupe quand ils se font envahir."
-			,"Leur abilité a s'installer la où on les attend le moins leur assure 1 pvic de + quand ils sont dans les couloirs"
+			,"Leur rapidité à juger leurs ennemis leur permet de se replier rapidement, ils ne perdent pas de troupe quand ils se font envahir."
+			,"Leur habilité a s'installer la où on les attend le moins leur assure 1 pvic de + quand ils sont dans les couloirs"
 			,"Ayant besoin des GI pour leurs travaux, Ce peuple ne peux pas les attaquer."
-			,"Leur examens pour devenir soldats est tellement soutenu et quotidien qu'une unité est réorientées à chaque tour vers un autre travail."
+			,"Leur examens pour devenir soldats est tellement soutenu et quotidien qu'une unité est réorientée à chaque tour vers un autre travail."
 			,"La chance ne leur est pas permise, ils ne peuvent donc pas compter sur le dé leur assurant l'espoir d'avoir des troupes en plus si ils attaquent avec un sous nombre."
 			,"D'une grande sauf de savoirs ils attaquent seulement les salles de cours."
 			,"Il faut 1 unités en + pour pouvoir les attaquer étant donné leur violence sans égal"
 			,"Etant doté d'un pass très efficace, Ils peuvent s'établir n'importe où"};
 	
 	private String listOfDescriptionsPower[] = {"+1 point de victoire bonus si installés dans la bibliothèque",
-			"+4 point de victoire si le foyer est possédé."
-			,"besoin d'1 d'attaque en plus mais jettent le dé à chaque fois"
+			"+4 points de victoire si le foyer est possédé."
+			,"a besoin d'1 d'attaque en plus pour attaquer mais jette le dé à chaque fois"
 			," à chaque tour, reçoivent une unité bonus"
-			,"+ 7 points lorsque la tribu sont choisis"
-			,"le peule a besoin de 1 troupes en moins pour attaquer"
-			,"résistant au attaque +1 unités adverses sont nécessaires pour les attaquer"
+			,"+ 7 points lorsque la tribu est choisie"
+			,"a besoin de 1 troupes en moins pour attaquer"
+			,"résistants au attaque +1 unités adverses sont nécessaires pour les attaquer"
 			,"leur attaque sournoise leur permet de ne pas céder de terrain dans les couloirs"};
 
 	public Bank(){
@@ -59,7 +59,7 @@ public class Bank implements TribeDeletedListener{
 		listOfPopulations.add(new Population("Informaticiens", listOfDescriptionsPop[2], 5, Population.TypePopulation.GI));
 		listOfPopulations.add(new Population("Disigners", listOfDescriptionsPop[3], 5, Population.TypePopulation.EDIM));
 		listOfPopulations.add(new Population("Environementalistes", listOfDescriptionsPop[4], 5, Population.TypePopulation.E));
-		listOfPopulations.add(new Population("Managers", listOfDescriptionsPop[5], 5, Population.TypePopulation.EDIM));
+		listOfPopulations.add(new Population("Managers", listOfDescriptionsPop[5], 5, Population.TypePopulation.IMSI));
 		listOfPopulations.add(new Population("Mecaniciens", listOfDescriptionsPop[6], 5, Population.TypePopulation.GMC));
 		listOfPopulations.add(new Population("Néophytes", listOfDescriptionsPop[7], 5, Population.TypePopulation.TC));
 		listOfPopulations.add(new Population("IUT", listOfDescriptionsPop[8], 5, Population.TypePopulation.IUT));
